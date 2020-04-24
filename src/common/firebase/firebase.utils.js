d2584a73ed32c8b25c113a3c52d0197df7407a6a
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-04-24 22:11:26 +0800

    Implemented Google Firebase

---
+++ src/common/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Set firebase config from dotenv
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_API_DATABASE_URL,
+  projectId: process.env.REACT_APP_API_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_APP_ID,
+  measurementId: process.env.REACT_APP_API_MEASUREMENT_ID
+};
+// Initialise firebase app
+firebase.initializeApp(config);
+// Declare firebase authentication and database
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export { auth, firestore };
