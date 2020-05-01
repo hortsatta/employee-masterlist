bb246bdb7f57910052f424dd5c22a918e70cfe60
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-01 09:35:08 +0800

    Added employee page to root routes

--- src/features/core/root.routes.js
+++ src/features/core/root.routes.js
@@ -1,3 +1,4 @@
+import { EmployeePage } from 'features/employee/pages';
 import OptionsPage from 'features/options/pages';
 
 export default {
@@ -6,6 +7,11 @@ export default {
     name: 'home',
     exact: true,
     component: null
+  },
+  employees: {
+    path: '/employees',
+    name: 'employees',
+    component: EmployeePage
   },
   options: {
     path: '/options',
