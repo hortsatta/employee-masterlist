e0c154f118374e517c567334ac9de15a2d052d59
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-01 10:46:27 +0800

    Added styles on selected item in gender-select component

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -3,3 +3,4 @@ export { default as InputGroups } from './input-groups/input-groups.component';
 export { default as WithDialog } from './with-dialog/with-dialog.component';
 export { default as WithProcessing } from './with-processing/with-processing.component';
 export { default as MomentDateInput } from './moment-date-input/moment-date-input.component';
+export { default as EmptyResult } from './empty-result/empty-result.component';
--- src/features/employee/components/gender-select/gender-select.component.js
+++ src/features/employee/components/gender-select/gender-select.component.js
@@ -6,30 +6,33 @@ import PropTypes from 'prop-types';
 
 const GENDERS = ['Male', 'Female'];
 
-const renderGender = (gender, { handleClick }) => (
-  <MenuItem
-    key={gender}
-    onClick={handleClick}
-    text={gender}
-  />
-);
-
-const GenderSelect = ({ gender, ...otherProps }) => (
-  <Select
-    fill
-    filterable={false}
-    items={GENDERS}
-    itemRenderer={renderGender}
-    {...otherProps}
-  >
-    <Button
-      fill
-      icon={IconNames.DELTA}
-      rightIcon={IconNames.CARET_DOWN}
-      text={gender || 'Gender'}
+const GenderSelect = ({ gender, ...otherProps }) => {
+  const genderRender = (item, { handleClick }) => (
+    <MenuItem
+      className={gender && gender === item && 'active'}
+      key={`item-${item}`}
+      onClick={handleClick}
+      text={item}
     />
-  </Select>
-);
+  );
+
+  return (
+    <Select
+      fill
+      filterable={false}
+      items={GENDERS}
+      itemRenderer={genderRender}
+      {...otherProps}
+    >
+      <Button
+        fill
+        icon={IconNames.DELTA}
+        rightIcon={IconNames.CARET_DOWN}
+        text={gender || 'Gender'}
+      />
+    </Select>
+  );
+};
 
 GenderSelect.propTypes = {
   gender: PropTypes.string
