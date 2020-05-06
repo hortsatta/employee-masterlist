82349ee953bb7af930c7b479b53ad7b83082cabe
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-06 09:01:02 +0800

    Added history button and salary field

--- src/features/employee/components/employee-info-form/employee-info-form.component.js
+++ src/features/employee/components/employee-info-form/employee-info-form.component.js
@@ -1,33 +1,61 @@
 import React from 'react';
-import { FormGroup } from '@blueprintjs/core';
+import { FormGroup, ControlGroup, NumericInput } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import PropTypes from 'prop-types';
 
-import { MomentDateInput } from 'common/components';
+import './employee-info-form.styles.scss';
+import { MomentDateInput, IconButton } from 'common/components';
 import DepartmentSelect from '../department-select/department-select.component';
 import JobTitleSelect from '../job-title-select/job-title-select.component';
 
+const HistoryButton = (props) => (
+  <IconButton content='View History' icon={IconNames.HISTORY} {...props} />
+);
+
 const EmployeeInfoForm = ({ fields, onChange }) => {
-  const { department, jobTitle, hireDate } = fields;
+  const { department, jobTitle, hireDate, salary } = fields;
   return (
     <form className='employee-info'>
       <FormGroup>
-        <DepartmentSelect
-          className='department-select'
-          department={department}
-          onItemSelect={(e) => onChange({ target: { value: e, name: 'department' } })}
-        />
-        <JobTitleSelect
-          jobTitle={jobTitle}
-          department={department}
-          onItemSelect={(e) => onChange({ target: { value: e, name: 'jobTitle' } })}
-        />
-        <MomentDateInput
-          placeholder='Hire Date'
-          onChange={(e) => onChange({ target: { value: e, name: 'hireDate' } })}
-          locale='en'
-          value={hireDate}
-          fill
-        />
+        <div className='row-2'>
+          <MomentDateInput
+            placeholder='Hire Date'
+            onChange={(e) => onChange({ target: { value: e, name: 'hireDate' } })}
+            locale='en'
+            value={hireDate}
+            fill
+          />
+          <ControlGroup fill>
+            <DepartmentSelect
+              className='department-select'
+              department={department}
+              onItemSelect={(e) => onChange({ target: { value: e, name: 'department' } })}
+            />
+            <HistoryButton />
+          </ControlGroup>
+        </div>
+        <div className='row-2'>
+          <ControlGroup fill>
+            <NumericInput
+              placeholder='Salary'
+              fill
+              allowNumericCharactersOnly
+              leftIcon={IconNames.DOLLAR}
+              min={0}
+              onValueChange={(e) => (!e && onChange({ target: { value: e, name: 'salary' } }))}
+              {...(salary ? { value: salary } : {})}
+            />
+            <HistoryButton />
+          </ControlGroup>
+          <ControlGroup fill>
+            <JobTitleSelect
+              jobTitle={jobTitle}
+              department={department}
+              onItemSelect={(e) => onChange({ target: { value: e, name: 'jobTitle' } })}
+            />
+            <HistoryButton />
+          </ControlGroup>
+        </div>
       </FormGroup>
     </form>
   );
@@ -37,7 +65,8 @@ EmployeeInfoForm.propTypes = {
   fields: PropTypes.shape({
     department: PropTypes.shape(),
     jobTitle: PropTypes.shape(),
-    hireDate: PropTypes.instanceOf(Date)
+    hireDate: PropTypes.instanceOf(Date),
+    salary: PropTypes.number
   }).isRequired,
   onChange: PropTypes.func.isRequired
 };
