2bcdf44cc50de10faa93c37673a3143ebff016f5
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-10 22:40:44 +0800

    Implemented employee picture upload to firebase storage

--- src/features/employee/employee.service.js
+++ src/features/employee/employee.service.js
@@ -1,4 +1,37 @@
-import { firebase, firestore } from 'common/utils';
+import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment';
+import Jimp from 'jimp';
+
+import { firebase, firestore, storage } from 'common/utils';
+
+const getRawBase64 = (base64) => base64.replace(/^data:image\/[a-z]+;base64,/, '');
+
+// Create image thumbnail with 64x64 size using Jimp
+const createThumbBase64 = async (rawBase64) => {
+  const thumbJimp = await Jimp.read(Buffer.from(rawBase64, 'base64'));
+  return thumbJimp.resize(64, 64).getBase64Async(Jimp.MIME_JPEG);
+};
+
+const saveEmployeePicture = async (picture, date) => {
+  // Create thumb image from employee picture
+  const rawBase64 = getRawBase64(picture.base64);
+  const thumbPictureBase64 = await createThumbBase64(rawBase64);
+  const rawThumbPictureBase64 = getRawBase64(thumbPictureBase64);
+
+  // Generate filename for picture using hire date and uuid and
+  // create file metadata including the content type
+  const filename = `${moment(date).format('YYYY-MM-DD')}_${uuidv4()}`;
+  // Set storage location to store file with its filename
+  const pictureRef = storage.child(`images/employee/${filename}`);
+  const thumbPictureRef = storage.child(`images/employee/${filename}_thumb`);
+  const metadata = { contentType: 'image/jpeg' };
+  // Upload picture using base64 image and
+  // return snapshot to retrieve image fullpath from storage
+  const pictureSnapshot = await pictureRef.putString(rawBase64, 'base64', metadata);
+  await thumbPictureRef.putString(rawThumbPictureBase64, 'base64', metadata);
+  const { metadata: { fullPath } } = pictureSnapshot;
+  return fullPath;
+};
 
 const createEmployeeDocument = async (employee) => {
   try {
@@ -15,9 +48,12 @@ const createEmployeeDocument = async (employee) => {
       emails,
       salary,
       jobTitle: { id: jobTitleId },
-      department: { id: departmentId }
+      department: { id: departmentId },
+      picture
     } = employee;
 
+    // Save employee picture to firebase storage and return fullpath
+    const pictureFullPath = await saveEmployeePicture(picture, hireDate);
     // Generate new doc and id, for parent employee,
     // Salary subcollection, department subcollection, and title subcollection
     const employeeDocRef = firestore.collection('employees').doc();
@@ -47,7 +83,9 @@ const createEmployeeDocument = async (employee) => {
         currentAddress,
         homeAddress,
         phones,
-        emails
+        emails,
+        picture: pictureFullPath,
+        isActive: true
       }
     };
 
