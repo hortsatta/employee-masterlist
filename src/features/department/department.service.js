dfc943332003e18ba4fa0f57d73142c1d9196963
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-15 18:11:31 +0800

    Added get department by id function to service

--- src/features/department/department.service.js
+++ src/features/department/department.service.js
@@ -1,17 +1,45 @@
-import { firestore } from 'common/utils';
+import { firestore, getDateFromTimestamp } from 'common/utils';
+
+const collectionName = 'departments';
 
 const getAllDepartments = async (isActive = true) => {
   try {
     const snapshots = await firestore
-      .collection('departments')
+      .collection(collectionName)
       .where('isActive', '==', isActive)
       .orderBy('name')
       .get();
 
-    return snapshots.docs.map((snapshot) => ({ ...snapshot.data(), id: snapshot.id }));
+    return snapshots.docs.map((snapshot) => {
+      const { createdAt, ...data } = snapshot.data();
+      return {
+        ...data,
+        id: snapshot.id,
+        createdAt: getDateFromTimestamp(createdAt)
+      };
+    });
   } catch (error) {
     throw error.message;
   }
 };
 
-export { getAllDepartments };
+const getDepartmentById = async (id) => {
+  try {
+    const snapshot = await firestore
+      .collection(collectionName)
+      .doc(id)
+      .get();
+
+    const { createdAt, ...data } = snapshot.data();
+
+    return {
+      ...data,
+      id: snapshot.id,
+      createdAt: getDateFromTimestamp(createdAt)
+    };
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export { getAllDepartments, getDepartmentById };
