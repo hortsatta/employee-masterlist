efebb414c1e2a69d02158f2276aa52504da48223
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-15 18:12:51 +0800

    Added get job title by id function

--- src/features/job-title/job-title.service.js
+++ src/features/job-title/job-title.service.js
@@ -1,17 +1,45 @@
-import { firestore } from 'common/utils';
+import { firestore, getDateFromTimestamp } from 'common/utils';
+
+const collectionName = 'titles';
 
 const getJobTitlesByDepartmentIds = async (ids, isActive = true) => {
   try {
     const snapshots = await firestore
-      .collection('titles')
+      .collection(collectionName)
       .where('departmentId', 'in', [...ids])
       .where('isActive', '==', isActive)
       .get();
 
-    return snapshots.docs.map((snapshot) => ({ ...snapshot.data(), id: snapshot.id }));
+    return snapshots.docs.map((snapshot) => {
+      const { createdAt, ...data } = snapshot.data();
+      return {
+        ...data,
+        id: snapshot.id,
+        createdAt: getDateFromTimestamp(createdAt)
+      };
+    });
   } catch (error) {
     throw error.message;
   }
 };
 
-export { getJobTitlesByDepartmentIds };
+const getJobTitleById = async (id) => {
+  try {
+    const snapshot = await firestore
+      .collection(collectionName)
+      .doc(id)
+      .get();
+
+    const { createdAt, ...data } = snapshot.data();
+
+    return {
+      ...data,
+      id: snapshot.id,
+      createdAt: getDateFromTimestamp(createdAt)
+    };
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export { getJobTitlesByDepartmentIds, getJobTitleById };
