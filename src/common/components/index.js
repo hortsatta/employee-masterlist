db6977557eadd2479c5896f28a56d9f2080c5fea
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-18 12:08:48 +0800

    Added with-spinner component

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -5,3 +5,4 @@ export { default as IconButton } from './icon-button/icon-button.component';
 export { default as InputGroups } from './input-groups/input-groups.component';
 export { default as LoadingBar } from './loading-bar/loading-bar.component';
 export { default as MomentDateInput } from './moment-date-input/moment-date-input.component';
+export { default as WithSpinner } from './with-spinner/with-spinner.component';
