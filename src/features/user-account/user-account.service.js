baa196f83bc46aaaef0dbfd519621db97d9c5694
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-22 11:00:21 +0800

    Implemented signout and get all user roles in service

--- src/features/user-account/user-account.service.js
+++ src/features/user-account/user-account.service.js
@@ -1,9 +1,11 @@
-import { firebase, auth, firestore } from 'common/utils';
+import { auth, firestore } from 'common/utils';
+
+const collectionName = 'users';
 
 const createUserDocument = async (user, moreData) => {
   if (!user) return;
 
-  const usersCollnRef = firestore.collection('users');
+  const usersCollnRef = firestore.collection(collectionName);
   const userDocRef = usersCollnRef.doc(user.uid);
   // Get specified user data
   const snapshot = await userDocRef.get();
@@ -11,18 +13,14 @@ const createUserDocument = async (user, moreData) => {
   // If user doc does not exists then create it
   if (!snapshot.exists) {
     const { email } = user;
-    const { employeeId, userRoleId } = moreData;
+    const { employeeId, userRole } = moreData;
     const batch = firestore.batch();
-    const fieldValue = firebase.firestore.FieldValue;
-    const userRoleDocRef = userDocRef.collection('userRole').doc();
 
     try {
       // Instantiate objects to be added to db
-      const newUser = { employeeId, email, isActive: true };
-      const newUserRole = { userRoleId, dateFrom: fieldValue.serverTimestamp() };
+      const newUser = { employeeId, email, userRole, isActive: true };
       // Add user and user role to batch and then commit
       batch.set(userDocRef, newUser);
-      batch.set(userRoleDocRef, newUserRole);
       await batch.commit();
     } catch (error) {
       throw error.message;
@@ -33,7 +31,7 @@ const createUserDocument = async (user, moreData) => {
 };
 
 const getUserSnapshotById = async (id) => (
-  firestore.collection('users').doc(id).get()
+  firestore.collection(collectionName).doc(id).get()
 );
 
 const getCurrentUser = async () => (
@@ -45,6 +43,14 @@ const getCurrentUser = async () => (
   })
 );
 
+const signOutUser = () => {
+  try {
+    auth.signOut();
+  } catch (error) {
+    throw error.message;
+  }
+};
+
 const signUpUser = async (credentials) => {
   try {
     const { email, password, ...moreData } = credentials;
@@ -55,4 +61,28 @@ const signUpUser = async (credentials) => {
   }
 };
 
-export { getUserSnapshotById, getCurrentUser, signUpUser };
+// Get all user roles from firestore
+const getAllUserRoles = async (isActive = true) => {
+  try {
+    const snapshots = await firestore
+      .collection('userRoles')
+      .where('isActive', '==', isActive)
+      .orderBy('value')
+      .get();
+
+    return snapshots.docs.map((snapshot) => ({
+      ...snapshot.data(),
+      id: snapshot.id
+    }));
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export {
+  getUserSnapshotById,
+  getCurrentUser,
+  signOutUser,
+  signUpUser,
+  getAllUserRoles
+};
