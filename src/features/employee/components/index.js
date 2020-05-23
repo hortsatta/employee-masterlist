8d86fb42e46259ccccee2bd618eb080b0883f05e
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-23 19:50:47 +0800

    Added employee info view component to employee drawer

--- src/features/employee/components/index.js
+++ src/features/employee/components/index.js
@@ -2,6 +2,7 @@ export { default as UpserEmployeeForm } from './upsert-employee-form/upsert-empl
 export { default as PersonalInfoForm } from './personal-info-form/personal-info-form.component';
 export { default as PersonalInfoView } from './personal-info-view/personal-info-view.component';
 export { default as EmployeeInfoForm } from './employee-info-form/employee-info-form.component';
+export { default as EmployeeInfoView } from './employee-info-view/employee-info-view.component';
 export { default as EmployeeListTable } from './employee-list-table/employee-list-table.component';
 export { default as EmployeeHistoryDialog } from './employee-history-dialog/employee-history-dialog.component';
 export { default as EmployeeViewDrawer } from './employee-view-drawer/employee-view-drawer.component';
