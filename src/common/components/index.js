2f842ede4ac5e116dbf522cf9576108e031ab176
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-23 11:11:58 +0800

    Added text field component

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -5,4 +5,5 @@ export { default as IconButton } from './icon-button/icon-button.component';
 export { default as InputGroups } from './input-groups/input-groups.component';
 export { default as LoadingBar } from './loading-bar/loading-bar.component';
 export { default as MomentDateInput } from './moment-date-input/moment-date-input.component';
+export { default as TextField } from './text-field/text-field.component';
 export { default as WithSpinner } from './with-spinner/with-spinner.component';
