496d14127f88f193a243122dcebbb567743ef04c
Erwin Errol Calanuga <hortsatta@gmail.com>
2020-05-26 19:33:42 +0800

    Added home page to main routing

--- src/features/core/root-routes.js
+++ src/features/core/root-routes.js
@@ -1,12 +1,14 @@
 import React, { lazy } from 'react';
 import { Route } from 'react-router-dom';
 
+import { HomePage } from 'features/home/pages';
+
 const EmployeePage = lazy(() => import('features/employee/pages').then(module => ({ default: module.EmployeePage })));
 const OptionsPage = lazy(() => import('features/options/pages'));
 
 const RootRoutes = () => (
   <>
-    <Route exact path='/' component={null} />
+    <Route exact path='/' component={HomePage} />
     <Route path='/employees' component={EmployeePage} />
     <Route exact path='/options' component={OptionsPage} />
   </>
